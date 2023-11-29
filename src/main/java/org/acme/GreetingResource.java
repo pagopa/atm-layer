66b60e6a12d7d4696628746edc78a18ff029f648
SMANUM <125576365+SMANUM@users.noreply.github.com>
2023-11-29 11:35:35 +0100

    Dev (#81)
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: GitHub Workflow <>
    Co-authored-by: Scarimbolo <michele.scarimbolo@nttdata.com>

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -4,10 +4,12 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.ResponseStatus;
 
 @Path("/microservice5/")
 public class GreetingResource {
 
+    @ResponseStatus(429)
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
